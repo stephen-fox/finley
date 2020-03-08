f68c4641d1d3efb5f2301df1586765247eafd6bd
Stephen Fox <stephen.j.fox.jr@gmail.com>
2020-03-08 12:57:52 -0400

    Initial support for concurrent decompilers.

--- cmd/ilspycmdw/main.go
+++ cmd/ilspycmdw/main.go
@@ -8,7 +8,9 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -19,6 +21,7 @@ func main() {
 	respectFileCase := flag.Bool("respect-file-case", false, "Respect filenames' case when matching their extensions")
 	noIlspyErrors := flag.Bool("no-ilspy-errors", false, "Exit if ILSpy extraction fails to extract a file")
 	scanRecursively := flag.Bool("r", false, "Scan recursively")
+	numDecompilers := flag.Int("num-workers", runtime.NumCPU(), "Number of .NET decompiler instances to run concurrently")
 
 	flag.Parse()
 
@@ -47,52 +50,63 @@ func main() {
 
 	start := time.Now()
 
+	d := newDoer(*numDecompilers)
+
 	err = filepath.Walk(*targetDirPath,
 		func(filePath string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if info.IsDir() {
-				return nil
-			}
 			if !*scanRecursively {
 				if filepath.Dir(filePath) != absTargetDirPath {
 					return filepath.SkipDir
 				}
 			}
 
-			for i := range fileExts {
+			d.queue(func() error {
+				if err != nil {
+					return err
+				}
+				if info.IsDir() {
+					return nil
+				}
 				filename := info.Name()
 				if !*respectFileCase {
 					filename = strings.ToLower(filename)
 				}
-				if strings.HasSuffix(filename, fileExts[i]) {
-					extractedDirPath, err := extractNETFile(extractInfo{
-						searchAbsPath:     absTargetDirPath,
-						targetFileAbsPath: filePath,
-						outputDirPath:     *outputDirPath,
-					})
-					if err != nil {
-						if _, ok := err.(*ilspyError); ok {
-							if *noIlspyErrors {
-								return err
+				for i := range fileExts {
+					if strings.HasSuffix(filename, fileExts[i]) {
+						extractedDirPath, err := extractNETFile(extractInfo{
+							searchAbsPath:     absTargetDirPath,
+							targetFileAbsPath: filePath,
+							outputDirPath:     *outputDirPath,
+						})
+						if err != nil {
+							if _, ok := err.(*ilspyError); ok {
+								if *noIlspyErrors {
+									return err
+								}
+								ioutil.WriteFile(filepath.Join(extractedDirPath, "extract-failure.log"), []byte(err.Error()), 0600)
+								log.Printf("[warn] %s", err.Error())
+								return nil
 							}
-							ioutil.WriteFile(filepath.Join(extractedDirPath, "extract-failure.log"), []byte(err.Error()), 0600)
-							log.Printf("[warn] %s", err.Error())
-							return nil
+							return fmt.Errorf("failed to extract '%s' - %s", filePath, err.Error())
 						}
-						return fmt.Errorf("failed to extract '%s' - %s", filePath, err.Error())
+
+						log.Printf("extracted '%s' to '%s'", filePath, extractedDirPath)
+
+						break
 					}
-
-					log.Printf("extracted '%s' to '%s'", filePath, extractedDirPath)
-
-					break
 				}
-			}
+				return nil
+			})
+
 			return nil
 		})
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+	}
+
+	err = d.wait()
+	if err != nil {
+		log.Fatalln(err.Error())
 	}
 
 	log.Printf("finished after %s", time.Since(start).String())
@@ -130,4 +144,60 @@ type ilspyError struct {
 
 func (o ilspyError) Error() string {
 	return o.err
+}
+
+func newDoer(numWorkers int) *doer {
+	pool := make(chan int, numWorkers)
+	for i := 0; i < numWorkers; i++ {
+		pool <- i
+	}
+
+	return &doer{
+		pool:   pool,
+		failed: make(chan error, 1),
+		dead:   make(chan struct{}),
+		wg:     &sync.WaitGroup{},
+	}
+}
+
+type doer struct {
+	pool   chan int
+	failed chan error
+	dead   chan struct{}
+	wg     *sync.WaitGroup
+}
+
+func (o *doer) queue(fn func() error) {
+	o.wg.Add(1)
+	go func() {
+		select {
+		case workerID := <-o.pool:
+			go func() {
+				err := fn()
+				if err != nil {
+					select {
+					case o.failed <- err:
+					default:
+						close(o.dead)
+					}
+					o.wg.Done()
+					return
+				}
+				o.wg.Done()
+				o.pool <- workerID
+			}()
+		case <-o.dead:
+			return
+		}
+	}()
+}
+
+func (o *doer) wait() error {
+	o.wg.Wait()
+	select {
+	case err := <-o.failed:
+		return err
+	default:
+		return nil
+	}
 }
