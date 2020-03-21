33c803744762efa92493e0ba408eaa92d5e60d7a
Stephen Fox <stephen.j.fox.jr@gmail.com>
2020-03-21 14:29:48 -0400

    Added a basic go doc.

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Package finley provides functionality that extends the CLI implementation of
+// ILSpy (https://github.com/icsharpcode/ILSpy). ILSply is an open source .NET
+// decompiler. It is normally invoked as a GUI application. However, a CLI
+// implementation ('ilspycmd') provides a better platform for automation.
+//
+// The primary features of finley include recursive decompilation of .NET
+// binaries in a given directory, duplicate .NET binary avoidance, and
+// configurable concurrent decompilation of .NET binaries.
+package finley
